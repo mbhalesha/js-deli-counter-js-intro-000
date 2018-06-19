258083de6cacb93dd35ab3019f97fbf6ce810a0e
Manisha Bhalesha <mbhalesha@gmail.com>
2018-06-19 16:11:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ function takeANumber (katzDeli, name) {
 }
 
 function nowServing(katzDeli) {
-  let i = 0;
-  while (i < katzDeli.length) {
-    i++;
-  }
+//  let i = 0;
+  //while (i < katzDeli.length) {
+    //i++;
+  //}
   if (katzDeli.length === 0) {
     return "There is nobody waiting to be served!";
   } else 
